d6244052968fab2dfc19e84135c362376c2edbbf
Adriane Caroline <adrianecaroline23@gmail.com>
2022-11-28 00:40:10 -0300

    modificacao tela inicial

--- src/Pages/Home/Initial.js
+++ src/Pages/Home/Initial.js
@@ -20,7 +20,7 @@ export function Initial () {
           <h3>
           Find the best books to read and make an amazing adventure through your imagination!
           </h3>
-          <button>Start</button>
+          <button onClick={() => {navigate("/profile")}}>Start</button>
         </div>
       </section>
     </Main>
